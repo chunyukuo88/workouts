3954828b350f82e447db8a0694c53cf28097e6bd
chunyukuo88 <alexgochenour@gmail.com>
2025-08-26 21:37:54 -0400

    feat: get workout by ID

---
+++ internal/api/workout_handler.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type WorkoutHandler struct{}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutId := chi.URLParam(r, "id")
+	if paramsWorkoutId == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutId, err := strconv.ParseInt(paramsWorkoutId, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "this is the workout id %d\n", workoutId)
+}
