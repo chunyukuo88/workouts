b1509fc79dd13ec17428a788a51d75b8284a03e3
chunyukuo88 <alexgochenour@gmail.com>
2025-08-29 19:00:54 -0400

    feat: database connection, yay

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,25 +1,37 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/chunyukuo88/workouts/internal/api"
+	"github.com/chunyukuo88/workouts/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
+	pgDB, err := store.Open()
+
+	if err != nil {
+		return nil, err
+	}
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
 	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
